40bd8af93d81147932673cd8f5895ed56420de72
Michał Białecki <mik.bialecki@gmail.com>
2018-04-09 06:47:02 +0200

    [minor] Added solution and projects

---
+++ MichalBialecki.com.OrleansCore.AccountTransfer.Grains/ServiceBusClient.cs
@@ -0,0 +1,23 @@
+﻿using MichalBialecki.com.OrleansCore.AccountTransfer.Interfaces;
+using Microsoft.Azure.ServiceBus;
+using System.Threading.Tasks;
+
+namespace MichalBialecki.com.OrleansCore.AccountTransfer.Grains
+{
+    public class ServiceBusClient : IServiceBusClient
+    {
+        private const string ServiceBusConnectionString = "Endpoint=sb://bialecki.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=[yourPriveteKey]";
+
+        private readonly TopicClient topicClient;
+
+        public ServiceBusClient()
+        {
+            topicClient = new TopicClient(ServiceBusConnectionString, "balanceUpdates");
+        }
+
+        public async Task SendMessageAsync(Message message)
+        {
+            await topicClient.SendAsync(message);
+        }
+    }
+}
